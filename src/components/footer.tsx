79ef803870a2c5a72e01be52a9ea65b05d82d4cc
donatas <donatas.andros@gmail.com>
2025-02-17 21:56:24 +0200

    refactor: give footer darker background and bigger padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="mt-16 w-full px-4 py-8 sm:px-6 md:mt-20 md:px-8">
+    <footer className="w-full py-12 bg-neutral-50 px-4 sm:px-6 md:px-8">
       <div className="mx-auto flex max-w-container flex-col-reverse justify-between md:flex-row">
         <p className="text-neutral-500">© 2025 Miami Car House</p>
         <ul className="mb-4 flex space-x-4 md:mb-0 *:text-neutral-600 *:font-semibold *:hover:text-neutral-700">
